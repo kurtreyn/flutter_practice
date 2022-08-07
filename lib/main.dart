09258969fe1a566b9e8fd61733dd8e94b3b2e0df
kurtreyn <kurtreyn@yahoo.com>
2022-08-07 19:48:56 -0400

    buttons and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   // const Home({Key? key}) : super(key: key);
@@ -15,11 +15,21 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
+      // ignore: prefer_const_constructors
       body: Center(
-        child: Image.asset('assets/astronaut.jpg')
+        child: ElevatedButton.icon(
+            onPressed: () {},
+            // ignore: prefer_const_constructors
+            icon: Icon(Icons.mail),
+            // ignore: prefer_const_constructors
+            label: Text('Mail me'),
+            // ignore: prefer_const_constructors
+            style: ElevatedButton.styleFrom(
+              primary: Colors.amber,
+            )),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
